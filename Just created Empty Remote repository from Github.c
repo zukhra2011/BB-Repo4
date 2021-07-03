9fbf6932605c9cdd3047c49c9eaf01dbc2132847
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-07-03 08:49:17 -0400

    Cloning procedure

---
+++ Just created Empty Remote repository from Github.c
@@ -0,0 +1,5 @@
+Just created Empty Remote repository from Github.com account by clicking ont eh plus icon on top right corner and new repo and give namee and click create.
+
+And Cloned this repo by clicking Steup in Desktop and selecting location click and clone.
+
+It clones our remote repo to local so we can start woking locally.
